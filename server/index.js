61e572fb5b87dd2ee721ca08f7fd005e4819a519
ahmed7761 <sharif7761@gmail.com>
2024-01-25 12:10:45 +0600

    project setup and database connection

---
+++ server/index.js
@@ -0,0 +1,20 @@
+// added "type": "module" - in package.json to import like this
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = process.env.CONNECT_DB;
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+
