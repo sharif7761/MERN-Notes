e9dc98a6299f4fd59d637d3574fc3190a2a35c84
ahmed7761 <sharif7761@gmail.com>
2024-01-25 18:32:32 +0600

    react app component design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
-function App() {
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/posts/Posts.jsx';
+import Form from './components/Form/Form';
+import useStyles from './styles';
+import memories from './images/memories.png';
+function App() {
+  const classes = useStyles();
   return (
-    <>
-        <h1>Hello React</h1>
-    </>
+      <Container maxWidth="lg">
+          <AppBar className={classes.appBar} position="static" color="inherit">
+              <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+              <img className={classes.image} src={memories} alt="icon" height="60" />
+          </AppBar>
+          <Container>
+              <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                  <Grid item xs={12} sm={7}>
+                      <Posts  />
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                      <Form  />
+                  </Grid>
+              </Grid>
+          </Container>
+      </Container>
   )
 }
 
